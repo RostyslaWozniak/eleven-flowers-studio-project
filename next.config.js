f482407604dbf88895993733dc6588b96797d082
Rostyslav Vozniak <rostik19wozniak@gmail.com>
2025-02-13 12:12:41 +0100

    try headers for images

--- next.config.js
+++ next.config.js
@@ -10,6 +10,19 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import("next").NextConfig} */
 const config = {
+  async headers() {
+    return [
+      {
+        source: "/images/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
