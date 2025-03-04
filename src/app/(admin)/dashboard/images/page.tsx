02f31154afdcef3d650dfad1393c7d8bd12e0c64
Rostyslav Vozniak <rostik19wozniak@gmail.com>
2025-03-04 19:33:51 +0100

    make page dynamic

--- src/app/(admin)/dashboard/images/page.tsx
+++ src/app/(admin)/dashboard/images/page.tsx
@@ -2,6 +2,8 @@ import { H1 } from "@/components/ui/typography";
 import { UploadImages } from "./_components/upload-images";
 import { ImageGallery } from "./_components/image-gallery";
 
+export const dynamic = "force-dynamic";
+
 export default async function Images() {
   return (
     <div className="w-full">
