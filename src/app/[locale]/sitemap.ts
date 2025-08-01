513fab84e15561a3192d2377d748936c1047ba6e
Rostyslav Vozniak <rostik19wozniak@gmail.com>
2025-08-01 11:21:15 +0200

    improve sitemap.xml

--- src/app/[locale]/sitemap.ts
+++ src/app/[locale]/sitemap.ts
@@ -3,43 +3,39 @@ import { api } from "@/trpc/server";
 import { type MetadataRoute } from "next";
 import { getLocale } from "next-intl/server";
 
-export const dynamic = "force-static";
-
-export const revalidate = 86400; // 1 day
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const collections = await api.public.collections.getAll({});
-  const { products } = await api.public.products.getAll({});
+  const { products } = await api.public.products.getAll({ take: 100 });
 
   const locale = await getLocale();
   const baseUrl = `${env.NEXT_PUBLIC_SERVER_URL}/${locale}`;
   const collectionPages: MetadataRoute.Sitemap = collections.map(
     (collection) => ({
       url: `${baseUrl}/collections/${collection.slug}`,
-      lastModified: new Date(),
+      priority: 0.6,
     }),
   );
 
   const productPages: MetadataRoute.Sitemap = products.map((product) => ({
     url: `${baseUrl}/collections/${product.collection?.slug}/${product.slug}`,
-    lastModified: new Date(),
+    priority: 0.8,
   }));
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      priority: 1,
     },
     {
       url: `${baseUrl}/contact`,
-      lastModified: new Date(),
+      priority: 0.5,
     },
     {
       url: `${baseUrl}/collections`,
-      lastModified: new Date(),
+      priority: 0.9,
     },
     {
       url: `${baseUrl}/products`,
-      lastModified: new Date(),
+      priority: 0.9,
     },
     ...collectionPages,
     ...productPages,
