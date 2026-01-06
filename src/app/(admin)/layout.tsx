c286d2cad9da520b3b7ca1ff46aad8a99d46a83b
Rostysalv <rostik19wozniak@gmail.com>
2026-01-06 12:41:54 +0100

    add ui information of screen operation

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 
 import { type Metadata } from "next";
 import { Sidebar } from "./_components/sidebar";
-import { Suspense } from "react";
 import { Toaster } from "sonner";
 import { Manrope, Philosopher } from "next/font/google";
 import { Providers } from "../providers";
@@ -11,6 +10,9 @@ import { getMessages, setRequestLocale } from "next-intl/server";
 import { EnvironmentBanner } from "@/components/environment-banner";
 import { getCurrentUser } from "@/auth/current-user";
 import { redirect } from "next/navigation";
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
+import { LogOutIcon, XIcon } from "lucide-react";
+import { LogOutButton } from "@/auth/components/log-out-button";
 export const metadata: Metadata = {
   title: {
     template: "Admin | %s | Eleven Flowers Studio",
@@ -63,18 +65,40 @@ export default async function AdminLayout({
             <Sidebar />
             <div className="relative grow px-10 py-12">{children}</div>
           </div>
-          <Suspense fallback={null}>
-            <Toaster
-              toastOptions={{
-                classNames: {
-                  error: "bg-destructive text-destructive-foreground",
-                  success: "bg-emerald-500 text-white",
-                  warning: "text-yellow-400",
-                  info: "bg-blue-400",
-                },
-              }}
-            />
-          </Suspense>
+
+          <Toaster
+            toastOptions={{
+              classNames: {
+                error: "bg-destructive text-destructive-foreground",
+                success: "bg-emerald-500 text-white",
+                warning: "text-yellow-400",
+                info: "bg-blue-400",
+              },
+            }}
+          />
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-background text-center xl:hidden">
+            <MaxWidthWrapper className="max-w-md">
+              <div className="flex flex-col items-center rounded-lg border border-dashed border-muted-foreground/50 bg-muted p-6">
+                <div className="mb-6 rounded-sm bg-destructive/60 p-2 text-destructive-foreground">
+                  <XIcon />
+                </div>
+                <div className="mb-6">
+                  <h1 className="text-bold mb-2 text-lg">
+                    Device not supported
+                  </h1>
+                  <p className="text-muted-foreground">
+                    {
+                      "This app doesn't work on phones or tablets. To continue, please use a computer or laptop."
+                    }
+                  </p>
+                </div>
+                <LogOutButton className="mt-4 w-full">
+                  <LogOutIcon />
+                  Logout
+                </LogOutButton>
+              </div>
+            </MaxWidthWrapper>
+          </div>
         </Providers>
       </body>
     </html>
