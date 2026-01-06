be93d49ea8a4155b6c7d26476378c5b25d4af87c
Rostysalv <rostik19wozniak@gmail.com>
2026-01-06 12:15:01 +0100

    fix nav item padding

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -4,90 +4,55 @@ import { getTranslations } from "next-intl/server";
 import RightNavPanel from "./right-nav-panel";
 import { Link, type Locale } from "@/i18n/routing";
 import { cn } from "@/lib/utils";
-// import {
-//   EnvironmentBanner,
-//   IS_LOCAL_PROJECT,
-//   IS_TEST_PROJECT,
-// } from "../environment-banner";
+
+const navigation = [
+  { id: 1, href: "/", label: "home" },
+  { id: 2, href: "/collections/bouquets", label: "bouquets" },
+  { id: 3, href: "/collections/gifts", label: "gifts" },
+  { id: 4, href: "/contact", label: "contact" },
+];
 
 export async function NavBar({ locale }: { locale: Locale }) {
   const t = await getTranslations({ namespace: "navigation", locale: locale });
 
   return (
-    <>
-      {/* <EnvironmentBanner /> */}
-      <header
-        className={cn(
-          "inset-x-0 top-0 z-50 w-screen bg-background/70 shadow-md shadow-foreground/20 backdrop-blur-sm md:sticky",
-          // {
-          //   "top-10": IS_TEST_PROJECT || IS_LOCAL_PROJECT,
-          // },
-        )}
-      >
-        <div className="mx-auto max-w-[1400px]">
-          <div className="flex h-14 items-center justify-between px-2">
-            <Link
-              href="/"
-              locale={locale}
-              aria-label="Logo Eleven Flowers Studio"
-            >
-              <Logo1 className="z-50 h-10 w-32 md:h-12" />
-            </Link>
+    <header
+      className={cn(
+        "inset-x-0 top-0 z-50 w-screen bg-background/70 shadow-md shadow-foreground/20 backdrop-blur-sm md:sticky",
+      )}
+    >
+      <div className="mx-auto max-w-[1400px]">
+        <div className="flex h-14 items-center justify-between px-2">
+          <Link
+            href="/"
+            locale={locale}
+            aria-label="Logo Eleven Flowers Studio"
+          >
+            <Logo1 className="z-50 h-10 w-32 md:h-12" />
+          </Link>
 
-            <nav className="itens-center hidden w-full md:flex">
-              <div className="flex h-full flex-grow justify-center">
-                <ul className="flex h-14">
-                  <li>
-                    <NavItem href="/">
+          <nav className="itens-center hidden w-full md:flex">
+            <div className="flex h-full flex-grow justify-center">
+              <ul className="flex h-14">
+                {navigation.map(({ id, href, label }) => (
+                  <li key={id}>
+                    <NavItem href={href}>
                       <Link
-                        href="/"
+                        href={href}
                         locale={locale}
-                        className="flex h-full items-center px-6"
+                        className="flex h-full items-center px-6 py-4"
                       >
-                        {t("home")}
+                        {t(label)}
                       </Link>
                     </NavItem>
                   </li>
-                  <li>
-                    <NavItem href="/collections/bouquets">
-                      <Link
-                        href="/collections/bouquets"
-                        locale={locale}
-                        className="flex h-full items-center px-4"
-                      >
-                        {t("bouquets")}
-                      </Link>
-                    </NavItem>
-                  </li>
-                  <li>
-                    <NavItem href="/collections/gifts">
-                      <Link
-                        href="/collections/gifts"
-                        locale={locale}
-                        className="flex h-full items-center px-4"
-                      >
-                        {t("gifts")}
-                      </Link>
-                    </NavItem>
-                  </li>
-                  <li>
-                    <NavItem href="/contact">
-                      <Link
-                        href="/contact"
-                        locale={locale}
-                        className="flex h-full items-center px-4"
-                      >
-                        {t("contact")}
-                      </Link>
-                    </NavItem>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-            <RightNavPanel />
-          </div>
+                ))}
+              </ul>
+            </div>
+          </nav>
+          <RightNavPanel />
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 }
