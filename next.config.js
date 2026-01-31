a3c99ab6ca05d170697b5cb21e3fe33d87d0ef85
Rostysalv <rostik19wozniak@gmail.com>
2026-01-31 18:41:51 +0100

    remove optimizing images by next.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 import "./src/env.js";
 
 import createNextIntlPlugin from "next-intl/plugin";
@@ -11,7 +7,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    unoptimized: true,
+    minimumCacheTTL: 31536000,
     remotePatterns: [
       {
         protocol: "https",
